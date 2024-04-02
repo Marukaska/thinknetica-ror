d7fbbd3e487c060bbd9e5265d1131cf2a068979e
Anton Dulnev <akaton@gmail.com>
2024-04-02 22:54:44 +0300

    Lesson 3 correction

--- lesson 3/exercise.rb
+++ lesson 3/exercise.rb
@@ -1,17 +1,19 @@
 class Station
-  attr_reader :trains, :trains_type, :station_name
-  def initialize(station_name)
-    @station_name = station_name
+  attr_reader :trains, :trains_type, :name
+
+  def initialize(name)
+    @name= name
     @trains = []
   end
+
   def train_take(train)
     @trains.append(train)
-    puts "Прибыл поезд: #{train.number}"
   end
+
   def train_send(train)
     @trains.delete(train)
-    puts "Отбывает поезд: #{train.number}"
   end
+
   def train_by_type
     @trains_type = {cargo:0, passenger:0}
     @trains.each do |train|
@@ -22,85 +24,78 @@ end
 
 class Route
   attr_reader :stations
+
   def initialize(start_station, end_station)
-    @start_station = start_station
-    @end_station = end_station
-    @interim_stations = []
+    @stations = [start_station, end_station]
   end
+
   def add_interim_stations(station)
-    @interim_stations.append(station)
-    puts "Добавлена промежуточная станция: #{station.station_name}"
+    @stations.insert(-2, station)
   end
+
   def delete_interim_stations(station)
-    @interim_stations.delete(station)
-    puts "Удалена промежуточная станция: #{station.station_name}"
-  end
-  def print_route
-    @stations = [@start_station, @interim_stations, @end_station].flatten
-    @stations.each { |st| puts "#{st.station_name}"}
+    @stations.delete(station)
   end
 end
 
 class Train
-  attr_reader :speed, :carriage_count, :number, :type
+  attr_accessor :speed
+
+  attr_reader :carriage_count, :number, :type
 
   TYPES = [:passenger, :cargo]
+
   def initialize(number, type, carriage_count)
     @number = number
     @type = TYPES[type]
     @carriage_count = carriage_count.to_i
     @speed = 0
   end
+
   def gain_speed(speed)
     @speed = speed
-    puts "Поезд разгоняется до #{speed} км\ч"
   end
+
   def stop_train
     @speed = 0
-    puts "Поезд остановлен"
   end
+
   def add_carriage
-    if @speed == 0
-      @carriage_count += 1
-      puts "К поезду добавлен один вагон, теперь вагонов #{@carriage_count}"
-    else
-      puts "Невозможно добавить вагон в движении"
-    end
+    @carriage_count += 1 if @speed == 0
   end
+
   def remove_carriage
-    if @speed == 0 && @carriage_count > 0
-      @carriage_count -= 1
-      puts "От поезда отцеплен один вагон, осталось вагонов #{@carriage_count}"
-    elsif @speed == 0 && @carriage_count == 0
-      puts "Все вагоны уже отцеплены от поезда"
-    else
-      puts "Невозможно добавить вагон в движении"
-    end
+    @carriage_count -= 1 if @speed == 0 && @carriage_count > 0
   end
+
   def add_rout(route)
     @route = route
     @current_station_index = 0
     current_station.train_take(self)
   end
+
   def move_next
     current_station.train_send(self)
-    @current_station_index += 1 if @current_station_index < @route.stations.size
+    @current_station_index += 1 if next_station
     current_station.train_take(self)
-    puts "Отправляемся на станцию #{@route.stations[@current_station_index].station_name}"
   end
+
   def move_previous
     current_station.train_send(self)
-    @current_station_index -= 1 if @current_station_index > 0
+    @current_station_index -= 1 if previous_station
     current_station.train_take(self)
-    puts "Отправляемся на станцию #{@route.stations[@current_station_index].station_name}"
   end
+
   def current_station
     @route.stations[@current_station_index]
   end
+
   def next_station
-    @route.stations[@current_station_index + 1] if @current_station_index < @route.stations.size
+    @route.stations[@current_station_index + 1]
   end
+
   def previous_station
     @route.stations[@current_station_index - 1] if @current_station_index > 0
   end
 end
+
