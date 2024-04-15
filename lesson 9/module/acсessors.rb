f73c686a4769121f93d89c44b316bf403b6abf56
Anton Dulnev <akaton@gmail.com>
2024-04-15 19:46:25 +0300

    Lesson 9

--- lesson 9/module/acсessors.rb
+++ lesson 9/module/acсessors.rb
@@ -6,13 +6,6 @@ module Acсessors
   end
 
   module ClassMethods
-    def my_attr_accessor(*names)
-      names.each do |name|
-        var_name = "@#{name}".to_sym
-        define_method(name) { instance_variable_get(var_name)}
-        define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
-      end
-    end
 
     def attr_accessor_with_history(*names)
       names.each do |name|
